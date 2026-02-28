1d7e3130a5f83394f28744b83dc64b86fe044f9f
Kevin Cameron <kevinacameron@hotmail.com>
2026-02-27 22:45:51 -0800

    next/prev dates

--- src/components/SessionNavigation/SessionNavigation.tsx
+++ src/components/SessionNavigation/SessionNavigation.tsx
@@ -6,36 +6,44 @@ import styles from './SessionNavigation.module.css';
 interface SessionNavigationProps {
   previousSession: string | null;
   nextSession: string | null;
+  previousDate: string | null;
+  nextDate: string | null;
   inline?: boolean;
 }
 
 export default function SessionNavigation({
   previousSession,
   nextSession,
+  previousDate,
+  nextDate,
   inline,
 }: SessionNavigationProps) {
   return (
     <Flex className={inline ? styles.navigationInline : styles.navigation} justify="between" align="center">
-      <div>
         {previousSession && (
-          <Button asChild variant="soft" size="2">
-            <Link href={`/${previousSession}`}>
-
-              Previous Session
+          <Button asChild variant="soft" size="3">
+            <Link href={`/${previousSession}`} className={styles.navButton}>
+              <div className={styles.buttonContent}>
+                <div>Previous Session</div>
+                {previousDate && (
+                  <div className={styles.dateLabel}>{previousDate}</div>
+                )}
+              </div>
             </Link>
           </Button>
         )}
-      </div>
-
-      <div>
         {nextSession && (
-          <Button asChild variant="soft" size="2">
-            <Link href={`/${nextSession}`}>
-              Next Session
+          <Button asChild variant="soft" size="3">
+            <Link href={`/${nextSession}`} className={styles.navButton}>
+              <div className={styles.buttonContent}>
+                <div>Next Session</div>
+                {nextDate && (
+                  <div className={styles.dateLabel}>{nextDate}</div>
+                )}
+              </div>
             </Link>
           </Button>
         )}
-      </div>
     </Flex>
   );
 }
